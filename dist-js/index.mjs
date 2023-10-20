5d938c5fe68167757c1ed70a3e3e5e788fa2db68
Lucas Fernandes Nogueira <lucas@tauri.app>
2023-10-20 16:51:44 +0000

    chore: update to tauri alpha.16, api alpha.9 (#673)
    Co-authored-by: Amr Bashir <amr.bashir2015@gmail.com>
    
    Committed via a GitHub action: https://github.com/tauri-apps/plugins-workspace/actions/runs/6590505706
    
    Co-authored-by: lucasfernog <lucasfernog@users.noreply.github.com>

--- dist-js/index.mjs
+++ dist-js/index.mjs
@@ -1,19 +1,21 @@
+import { invoke } from '@tauri-apps/api/primitives';
+
 // Copyright 2019-2023 Tauri Programme within The Commons Conservancy
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 class Authenticator {
     async init() {
-        return await window.__TAURI_INVOKE__("plugin:authenticator|init_auth");
+        return await invoke("plugin:authenticator|init_auth");
     }
     async register(challenge, application) {
-        return await window.__TAURI_INVOKE__("plugin:authenticator|register", {
+        return await invoke("plugin:authenticator|register", {
             timeout: 10000,
             challenge,
             application,
         });
     }
     async verifyRegistration(challenge, application, registerData, clientData) {
-        return await window.__TAURI_INVOKE__("plugin:authenticator|verify_registration", {
+        return await invoke("plugin:authenticator|verify_registration", {
             challenge,
             application,
             registerData,
@@ -21,7 +23,7 @@ class Authenticator {
         });
     }
     async sign(challenge, application, keyHandle) {
-        return await window.__TAURI_INVOKE__("plugin:authenticator|sign", {
+        return await invoke("plugin:authenticator|sign", {
             timeout: 10000,
             challenge,
             application,
@@ -29,7 +31,7 @@ class Authenticator {
         });
     }
     async verifySignature(challenge, application, signData, clientData, keyHandle, pubkey) {
-        return await window.__TAURI_INVOKE__("plugin:authenticator|verify_signature", {
+        return await invoke("plugin:authenticator|verify_signature", {
             challenge,
             application,
             signData,
