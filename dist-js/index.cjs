dc46aeeb46f98d585749453629aa22fcd0ca3577
Lucas Fernandes Nogueira <lucas@tauri.app>
2023-12-19 22:18:58 +0000

    chore(deps): update to tauri alpha.20, @tauri-apps/api alpha.13 (#839)
    * chore(deps): update to tauri alpha.20, @tauri-apps/api alpha.13
    
    * fix lockfile
    
    Committed via a GitHub action: https://github.com/tauri-apps/plugins-workspace/actions/runs/7268221303
    
    Co-authored-by: amrbashir <amrbashir@users.noreply.github.com>

--- dist-js/index.cjs
+++ dist-js/index.cjs
@@ -1,23 +1,23 @@
 'use strict';
 
-var primitives = require('@tauri-apps/api/primitives');
+var core = require('@tauri-apps/api/core');
 
 // Copyright 2019-2023 Tauri Programme within The Commons Conservancy
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 class Authenticator {
     async init() {
-        return await primitives.invoke("plugin:authenticator|init_auth");
+        return await core.invoke("plugin:authenticator|init_auth");
     }
     async register(challenge, application) {
-        return await primitives.invoke("plugin:authenticator|register", {
+        return await core.invoke("plugin:authenticator|register", {
             timeout: 10000,
             challenge,
             application,
         });
     }
     async verifyRegistration(challenge, application, registerData, clientData) {
-        return await primitives.invoke("plugin:authenticator|verify_registration", {
+        return await core.invoke("plugin:authenticator|verify_registration", {
             challenge,
             application,
             registerData,
@@ -25,7 +25,7 @@ class Authenticator {
         });
     }
     async sign(challenge, application, keyHandle) {
-        return await primitives.invoke("plugin:authenticator|sign", {
+        return await core.invoke("plugin:authenticator|sign", {
             timeout: 10000,
             challenge,
             application,
@@ -33,7 +33,7 @@ class Authenticator {
         });
     }
     async verifySignature(challenge, application, signData, clientData, keyHandle, pubkey) {
-        return await primitives.invoke("plugin:authenticator|verify_signature", {
+        return await core.invoke("plugin:authenticator|verify_signature", {
             challenge,
             application,
             signData,
